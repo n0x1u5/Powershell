a5cf5a8ff27ea8eb63e0b50f8097a9b7ffe62683
n0xi <toteking1520@gmail.com>
2022-08-27 13:09:19 +0200

    Add files via upload

---
+++ HappyBirthday-song.ps1
@@ -0,0 +1,35 @@
+﻿$BeepList = @(
+    @{ Pitch = 1000; Length = 300; };
+    @{ Pitch = 1000; Length = 200; };
+    @{ Pitch = 1150; Length = 500; };
+    @{ Pitch = 1000; Length = 500; };
+    @{ Pitch = 1350; Length = 500; };
+    @{ Pitch = 1250; Length = 800; };
+
+    @{ Pitch = 1000; Length = 300; };
+    @{ Pitch = 1000; Length = 200; };
+    @{ Pitch = 1150; Length = 500; };
+    @{ Pitch = 1000; Length = 500; };
+    @{ Pitch = 1500; Length = 500; };
+    @{ Pitch = 1350; Length = 800; };
+
+    @{ Pitch = 1000; Length = 300; };
+    @{ Pitch = 1000; Length = 200; };
+    @{ Pitch = 2000; Length = 500; };
+    @{ Pitch = 1700; Length = 500; };
+    @{ Pitch = 1370; Length = 500; };
+    @{ Pitch = 1225; Length = 500; };
+    @{ Pitch = 1150; Length = 500; };
+    @{ Pitch = 1800; Length = 300; };
+    @{ Pitch = 1800; Length = 200; };
+    @{ Pitch = 1700; Length = 500; };
+    @{ Pitch = 1370; Length = 700; };
+    @{ Pitch = 1500; Length = 500; };
+    @{ Pitch = 1370; Length = 500; };
+    );
+
+foreach ($Beep in $BeepList) {
+    [System.Console]::Beep($Beep['Pitch'], $Beep['Length']);
+}
+
+echo "FELIZ CUMPLEAÑOS ALEX !! "
